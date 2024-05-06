720c792230cd53c63017a96ef42c8430c39e609e
stan4u <stan4u@etu.univ-lorraine.fr>
2024-05-06 09:50:00 +0200

    Initial commit

---
+++ inc.js
@@ -0,0 +1,8 @@
+function include(contenu) {
+	console.log(contenu);
+	fetch(contenu).then(function (response) {
+	return response.text();
+}).then(function (html) {
+document.getElementById('include').innerHTML=html;
+});
+}
